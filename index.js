b4c4e717916b1337d7c22fb79c678f19b0b786de
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-12-22 00:15:17 -0500

    commit tree de2d836e61d27fcc7e6b67d202394c62714200d8

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => string.split(' ')).map(function (word) {
+    let result = [];
+    for (let i = 0; i < word.length; i++) {
+      result.push(word[i][0].toUpperCase() + word[i].substr(1));
+    }
+    return result.join(' ');
+    })
 }
+console.log(titleCased(tutorials));
+// let result = titleCased(tutorials);
+// console.log(result);
+
+// let map1 = tutorials.map(string => string.split(' '));
+// let map2 = map1.map(function (word) {
+//   let result = [];
+//   for (let i = 0; i < word.length; i++) {
+//     result.push(word[i][0].toUpperCase() + word[i].substr(1));
+//   }
+//   return result.join(' ');
+//   })
+
+
+
+
+// const titleCased = tutorials.map(
+//   function capitalize (string) {
+//     let words = string.split(' ');
+//     let stringArray = [];
+//     for (i = 0; i < words.length; i++) {
+//       let firstOfString = words[i][0].toUpperCase();
+//       let restOfString = words[i].substr(1);
+//       stringArray.push(firstOfString + restOfString);
+//     }
+//     return stringArray.join(' ');
+//   }
+// )
+// console.log(titleCased);
+
+
